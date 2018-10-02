4c7bd6fc4c3f1303056fc5d59f18aa1af49de823
ShemikaKW <Shemikakw@gmail.com>
2018-10-02 18:26:13 -0400

    Final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,21 +12,9 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
 
-    // Variables
-
-    var trainName = "";
-    var trainDestination = "";
-    var firstTrainTime = "";
-    var trainFrequency = "";
-
-    // Time Conversion
-    var firstTimeConverted = "";
-    var diffTime = "";
-    var timeRemainder;
-    var minutesTilTrain;
-    var nextTrain;
 
     var database = firebase.database();
+    // database.ref().remove();
 
     // Functions
 
@@ -34,11 +22,35 @@ $(document).ready(function () {
     $("#add-train").click(function (event) {
         event.preventDefault();
 
+        // Clears message
+        $("#message").text("");
+
         // Get User Input Information
         var trainName = $("#name-input").val().trim();
         var trainDestination = $("#destination-input").val().trim();
         var firstTrainTime = $("#first-train-input").val().trim();
         var trainFrequency = $("#frequency-input").val().trim();
+
+
+        // Time conversion
+        // Convert to HH:MM; (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years").format("X");
+        if (trainName === "" && trainDestination === "" && firstTrainTime === "" && trainFrequency === "") {
+            $("#message").text("Please enter text in all fields");
+            return;
+        }
+        if (firstTimeConverted === "Invalid date") {
+            $("#message").text("Must use Military Time");
+            return;
+        }
+
+        database.ref().push({
+            name: trainName,
+            destination: trainDestination,
+            firstTrain: firstTimeConverted,
+            frequency: trainFrequency,
+
+        });
 
         // Remove User Input Information
         $("#name-input").val("");
@@ -46,38 +58,7 @@ $(document).ready(function () {
         $("#first-train-input").val("");
         $("#frequency-input").val("");
 
-        // Time conversion
-        // Convert to HH:MM; (pushed back 1 year to make sure it comes before current time)
-        firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
 
-        // Current Time
-        var currentTime = moment();
-
-        //Difference between the times
-        diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-        // Time apart (remainder)
-        timeRemainder = diffTime % trainFrequency;
-
-        // Minutes Until Next Train
-        minutesTilTrain = trainFrequency - timeRemainder;
-
-        // Next Train
-        nextTrain = moment().add(minutesTilTrain, "minutes");
-        nextTrainFormat = moment(nextTrain).format("hh:mm");
-
-
-        if (trainName != "" && trainDestination != "" && firstTrainTime != "" && trainFrequency != "") {
-            database.ref().push({
-                name: trainName,
-                destination: trainDestination,
-                firstTrain: firstTrainTime,
-                frequency: trainFrequency,
-
-            });
-        } else {
-            $("#message").text("Please enter text in all fields");
-        }
     });
 
     // Create Firebase event for adding train to the database and a row in the html when user adds an entry
@@ -89,12 +70,29 @@ $(document).ready(function () {
         var firstTrainTime = childSnapshot.val().firstTrain;
         var trainFrequency = childSnapshot.val().frequency;
 
+        var firstTimeConverted = moment(firstTrainTime, "hh:mm");
+        // Current Time
+        var currentTime = moment();
+
+        //Difference between the times
+        var diffTime = currentTime.diff(firstTimeConverted, "minutes");
+
+        // Time apart (remainder)
+        var timeRemainder = diffTime % trainFrequency;
+
+        // Minutes Until Next Train
+        var minutesTilTrain = trainFrequency - timeRemainder;
+
+        // Next Train
+        var nextTrain = currentTime.add(minutesTilTrain, "minutes");
+
+
         // Create the new row
         var newRow = $("<tr>").append(
             $("<td>").text(trainName),
             $("<td>").text(trainDestination),
             $("<td>").text(trainFrequency + " minutes"),
-            $("<td>").text(moment(nextTrain).calendar()),
+            $("<td>").text(nextTrain.calendar()),
             $("<td>").text(minutesTilTrain + " minutes")
         );
 
